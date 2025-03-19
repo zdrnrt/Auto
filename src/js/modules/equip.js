3da1729cae266b8ff175749614bc3e71cdd9c8ec
Задорин Артем <artfan95@gmail.com>
2025-03-20 00:57:03 +0700

    Страница подачи данных

--- src/js/modules/equip.js
+++ src/js/modules/equip.js
@@ -1,3 +1,14 @@
 window.equipOpen = function(){
   moduleOpen('./src/html/equip.html')
 }
+
+window.equipDownload = (e) => {
+  e.preventDefault();
+  const format = e.target.querySelector('[name="format"]:checked').value;
+  console.log(format)
+}
+
+window.equipFilter = (e) => {
+  e.preventDefault();
+  console.log(e.target)
+}
