416e5e0adde2333efdfa88f667d7dd9041083921
Задорин Артем <artfan95@gmail.com>
2025-03-21 01:19:47 +0700

    Доработки страницы и окна с фильтрами

--- src/js/modules/equip.js
+++ src/js/modules/equip.js
@@ -1,5 +1,18 @@
+import {Modal} from 'bootstrap'
+
 window.equipOpen = function(){
   moduleOpen('./src/html/equip.html')
+  .then( () => {
+      
+      window.equipUpdateModal = new Modal(document.getElementById('euqipUpdateModal'), {
+        backdrop: true, keyboard: true
+      })
+      document.getElementById('equipUpdateOpen').addEventListener('click', ()=>{
+        window.equipUpdateModal.show();
+      })
+});
+
+
 }
 
 window.equipDownload = (e) => {
@@ -11,4 +24,10 @@ window.equipDownload = (e) => {
 window.equipFilter = (e) => {
   e.preventDefault();
   console.log(e.target)
+}
+
+window.equipUpdate = (e) => {
+  e.preventDefault();
+  console.log(e.target)
+  window.equipUpdateModal.hide()
 }
